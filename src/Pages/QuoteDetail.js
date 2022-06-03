3b12236bc9aa0d95690a169dee41a23bb30a6f15
Tushar <tushar.nandy@vidyamantra.com>
2022-06-03 10:22:20 +0530

    created dummy page components

---
+++ src/Pages/QuoteDetail.js
@@ -0,0 +1,4 @@
+const QipteDetails = () => {
+    return <h1>Qipte Details</h1>
+}
+export default QipteDetails
--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -2,14 +2,13 @@ import { useState } from 'react';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
-
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
