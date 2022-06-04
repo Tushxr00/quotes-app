e9899e452ed2c1baba9b7dbac9248bc995227346
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-05 01:12:07 +0530

    highlighting the quote on full screen

--- src/Pages/QuoteDetail.js
+++ src/Pages/QuoteDetail.js
@@ -1,18 +1,22 @@
 import { Fragment } from "react";
 import { Route, Routes, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import dummyQuotes from "../components/data/dummyQuotes";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
-const QipteDetails = () => {
+const QuoteDetail = () => {
   const params = useParams();
-
+  const quote = dummyQuotes.find((quote) => quote.id === params.quoteId);
+  if (!quote) {
+    return <p>no Quoute found!</p>;
+  }
   return (
     <Fragment>
-      <h1>Qipte Details</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Routes>
         <Route path="comments" element={<Comments />} />
       </Routes>
     </Fragment>
   );
 };
-export default QipteDetails;
+export default QuoteDetail;
