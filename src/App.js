539ab0337a9e99ec37a1a17044b687b46fe0e772
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-04 19:19:30 +0530

    made a header and a layout wrapper class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./Pages/AllQuotes";
 import NewQuote from "./Pages/NewQuote";
 import QuoteDetals from "./Pages/QuoteDetail";
 
 function App() {
   return (
-    <div>
+    <Layout>
       <Routes>
         <Route path="/" element={<Navigate to="/quotes" />} />
         <Route path="/quotes" element={<AllQuotes />} />
         <Route path="/quotes/:quoteId/*" element={<QuoteDetals />} />
         <Route path="/new-quote" element={<NewQuote />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
