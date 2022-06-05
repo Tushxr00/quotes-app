06b1f613f87c2fbddb99a1f9ea8d4e032c591a41
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-05 20:32:06 +0530

    applying query params

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,29 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const navigation = useNavigate();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const changeSortingHandler = () => {
+    navigation(`/quotes?sort=${isSortingAscending ? "desc" : "asc"}`);
+    console.log(location);
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
@@ -21,3 +39,4 @@ const QuoteList = (props) => {
 };
 
 export default QuoteList;
+
