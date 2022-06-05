dbd53c77f3c2b9c237b7ad6ee19a11bfe7ff2d1b
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-05 23:59:13 +0530

    sending  dummy data to firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import dummyQuotes from "./components/data/dummyQuotes";
 import Layout from "./components/layout/Layout";
+import { addQuote } from "./lib/api";
 import AllQuotes from "./Pages/AllQuotes";
 import NewQuote from "./Pages/NewQuote";
 import NotFound from "./Pages/NotFound";
 import QuoteDetals from "./Pages/QuoteDetail";
+import useHttp from "./hooks/use-http";
 
 function App() {
+  /**
+   * code to send dummy data in firebase
+   */
+  // const { sendRequest, status } = useHttp(addQuote);
+  // console.log(dummyQuotes.length);
+  // useEffect(() => {
+  //   for (let i = 0; i < dummyQuotes.length; i++)
+  //     sendRequest({
+  //       text: dummyQuotes[i].text,
+  //       author: dummyQuotes[i].author,
+  //     });
+  // }, []);
   return (
     <Layout>
       <Routes>
