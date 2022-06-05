412699b92c07a4e188548df262b943f8ee8e923a
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-05 20:37:55 +0530

    sorting quotes

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -4,6 +4,16 @@ import { useLocation, useNavigate } from "react-router-dom";
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
   const navigation = useNavigate();
   const location = useLocation();
@@ -12,9 +22,10 @@ const QuoteList = (props) => {
 
   const isSortingAscending = queryParams.get("sort") === "asc";
 
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
   const changeSortingHandler = () => {
     navigation(`/quotes?sort=${isSortingAscending ? "desc" : "asc"}`);
-    console.log(location);
   };
 
   return (
@@ -25,7 +36,7 @@ const QuoteList = (props) => {
         </button>
       </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
+        {sortedQuotes.map((quote) => (
           <QuoteItem
             key={quote.id}
             id={quote.id}
