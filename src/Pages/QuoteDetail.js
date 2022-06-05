621431239a5cc2ac869f6e36e82e15edeb0ce1b3
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-05 20:47:06 +0530

    using nested routes for optional rendering

--- src/Pages/QuoteDetail.js
+++ src/Pages/QuoteDetail.js
@@ -13,12 +13,20 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <div className="centered">
-        <Link to={`/quotes/${params.quoteId}/comments`} className="btn--flat">
-          Load comments
-        </Link>
-      </div>
       <Routes>
+        <Route
+          path="*"
+          element={
+            <div className="centered">
+              <Link
+                to={`/quotes/${params.quoteId}/comments`}
+                className="btn--flat"
+              >
+                Load comments
+              </Link>
+            </div>
+          }
+        />
         <Route path="comments" element={<Comments />} />
       </Routes>
     </Fragment>
