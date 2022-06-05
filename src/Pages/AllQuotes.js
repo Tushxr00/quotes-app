31463ef7b8ffe069a97e0c5ebdfedc43456c127a
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-06-05 23:39:40 +0530

    adding data from fire base

--- src/Pages/AllQuotes.js
+++ src/Pages/AllQuotes.js
@@ -1,6 +1,37 @@
+import { useEffect } from "react";
 import dummyQuotes from "../components/data/dummyQuotes";
 import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import NoQuoteFound from "../components/quotes/NoQuotesFound";
+
 const AllQuotes = () => {
-  return <QuoteList quotes={dummyQuotes} />;
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getAllQuotes, true);
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className="centered focused">{error}</div>;
+  }
+
+  if (status === "completed" && (!loadedQuote || loadedQuote.length === 0)) {
+    return <NoQuoteFound />;
+  }
+  return <QuoteList quotes={loadedQuote} />;
 };
 export default AllQuotes;
