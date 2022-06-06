a13c617d8734ca1828f37fde3cc14fe00e3b30ec
Tushar <tushar.nandy@vidyamantra.com>
2022-06-06 15:40:02 +0530

    applied lazy loading to improve the performance

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import dummyQuotes from "./components/data/dummyQuotes";
 import Layout from "./components/layout/Layout";
 import { addQuote } from "./lib/api";
-import AllQuotes from "./Pages/AllQuotes";
-import NewQuote from "./Pages/NewQuote";
-import NotFound from "./Pages/NotFound";
-import QuoteDetals from "./Pages/QuoteDetail";
 import useHttp from "./hooks/use-http";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+/**
+ * normal import statement
+ */
+// import AllQuotes from "./Pages/AllQuotes";
+// import NewQuote from "./Pages/NewQuote";
+// import NotFound from "./Pages/NotFound";
+// import QuoteDetals from "./Pages/QuoteDetail";
+
+/**
+ * lazy loading the components to improve the performance
+ */
+const NewQuote = React.lazy(() => import('./Pages/NewQuote'))
+const AllQuotes = React.lazy(() => import("./Pages/AllQuotes"))
+const NotFound = React.lazy(() => import("./Pages/NotFound"))
+const QuoteDetals = React.lazy(() => import("./Pages/QuoteDetail"))
 
 function App() {
   /**
@@ -24,13 +36,19 @@ function App() {
   // }, []);
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to="/quotes" />} />
-        <Route path="/quotes" element={<AllQuotes />} />
-        <Route path="/quotes/:quoteId/*" element={<QuoteDetals />} />
-        <Route path="/new-quote" element={<NewQuote />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={
+        <div className="centered">
+          <LoadingSpinner/>
+        </div>
+      }>
+        <Routes>
+          <Route path="/" element={<Navigate to="/quotes" />} />
+          <Route path="/quotes" element={<AllQuotes />} />
+          <Route path="/quotes/:quoteId/*" element={<QuoteDetals />} />
+          <Route path="/new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
